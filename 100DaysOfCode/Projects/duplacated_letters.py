96ac892d5372c5d8b58263f5137d0ee24c6d50bc
Arouna Faical KINDA <arounakinda@gmail.com>
2025-10-11 21:05:21 +0200

    find duplicated in list whithout using sets

---
+++ 100DaysOfCode/Projects/duplacated_letters.py
@@ -0,0 +1,18 @@
+# This program finds duplicated letters in a list without using sets
+some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
+duplicated_items = []
+list_len = len(some_list)
+
+i = 0
+while i < list_len:
+    process_letter = some_list[i]
+    print(process_letter)
+    for letter in some_list[i + 1 :]:
+        print(letter, end=", ")
+        if letter == process_letter:
+            duplicated_items.append(process_letter)
+    i = i + 1
+
+print("The duplicated letters are", end=": ")
+for letter in duplicated_items:
+    print(letter, end=" ")
